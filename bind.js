8f237b4832df4c6c0175752f5a0a4d8fa13b460e
Roman Zhak <zhak55@ya.ru>
2014-04-25 23:35:10 +0400

    Update bind.js

--- bind.js
+++ bind.js
@@ -1,13 +1,11 @@
-Function.prototype.bind = Function.prototype.bind || function( context ){
-	var  func   = typeof this === 'function' ? this : 0
-	   , sliced = Array.prototype.slice
-	   , args   = sliced.call( arguments, 1 );
-
-	 if( func ) {
-	 	return function() {
-	 		return func.apply( context, args.concat( sliced.call( arguments )));
-	 	}
-	 }else {
-	 	throw new TypeError("Must be a function");
-	 }
-}
+Function.prototype.bind = 
+  Function.prototype.bind || function( context ){
+    var  func = typeof this === 'function' ? this : 0
+        , sliced = Array.prototype.slice
+        , args   = sliced.call( arguments, 1 );
+        
+     if( !func ) throw new TypeError("Must be a function");
+	return function() {
+ 	  return func.apply( context, args.concat( sliced.call( arguments )));
+      }
+    }
